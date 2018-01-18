ce2be8a18ad6bd7c146bcc0b090632d5a618950c
Makoto Tanabe <xekid78@outlook.com>
2018-01-18 09:36:15 +0900

    for文

---
+++ sample7.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class sample7 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 12; i++) {
+			System.out.println(i + "月");
+		}
+
+	}
+
+}
