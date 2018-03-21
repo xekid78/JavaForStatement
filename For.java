065a9ad73dd94cb3d26cc7b05726a2a7195c68ea
Makoto Tanabe <xekid78@outlook.com>
2018-03-21 12:21:50 +0900

    Add files via upload

---
+++ For.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class For {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 12; i++) {
+			System.out.println(i + "月");
+		}
+
+	}
+
+}
